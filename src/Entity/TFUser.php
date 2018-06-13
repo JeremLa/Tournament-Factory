12216197b236f42bd215a15572358bf3b5a9e322
JeremLa <jeremy.lahore@gmail.com>
2018-06-13 07:07:13 +0200

    New : TFUser
    Create TFUser entity whith all properties for create a user for tournament participation, Email is unique and will be used for get back historical when he will matched to new account will be created

--- src/Entity/TFUser.php
+++ src/Entity/TFUser.php
@@ -17,11 +17,31 @@ class TFUser
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=true, unique=true)
      */
     private $email;
 
-    public function getId()
+    /**
+     * @ORM\Column(name="nicknames", type="array", nullable=true)
+     */
+    private $nicknames;
+
+    /**
+     * @ORM\Column(name="firstname", type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(name="lastname", type="string", length=255, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(name="country", type="string", length=255, nullable=true)
+     */
+    private $country;
+
+    public function getId() : int
     {
         return $this->id;
     }
@@ -36,5 +56,69 @@ class TFUser
         $this->email = $email;
 
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNicknames()
+    {
+        return $this->nicknames;
+    }
+
+    /**
+     * @param mixed $nicknames
+     */
+    public function setNicknames($nicknames): void
+    {
+        $this->nicknames = $nicknames;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country): void
+    {
+        $this->country = $country;
     }
 }
