1cfed8475a4245cdf839f6fe331f076775045e66
ahermes <hermesalexis@gmail.com>
2018-06-25 13:54:20 +0200

    {#53] ADD : Tournament Service
    Creating service for the tournament controller

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -6,6 +6,7 @@ use App\Entity\TFTournament;
 use App\Entity\TFUser;
 use App\Repository\TFTournamentRepository;
 use App\Repository\TFUserRepository;
+use App\Services\TournamentService;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
@@ -18,13 +19,13 @@ use Symfony\Component\VarDumper\VarDumper;
 class TournamentController extends Controller
 {
     /**
-     * @var EntityManagerInterface $entityManger
+     * @var EntityManagerInterface $entityManager
      */
-    private $entityManger;
+    private $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->entityManger = $entityManager;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -35,7 +36,7 @@ class TournamentController extends Controller
         /**
          * @var TFTournament[] $tournaments
          */
-        $tournaments = $this->entityManger->getRepository('App\Entity\TFTournament')->findAll();
+        $tournaments = $this->entityManager->getRepository('App\Entity\TFTournament')->findAll();
 
         return $this->render('tournament/index.html.twig', [
             'controller_name' => 'TournamentController',
@@ -62,8 +63,8 @@ class TournamentController extends Controller
         if($form->isSubmitted() && $form->isValid()) {
             $tournament->setOwner($this->getUser()->getTFUser());
 
-            $this->entityManger->persist($tournament);
-            $this->entityManger->flush();
+            $this->entityManager->persist($tournament);
+            $this->entityManager->flush();
 
             $this->addFlash('success', 'tournament.new.message');
 
@@ -81,11 +82,11 @@ class TournamentController extends Controller
      */
     public function removeTournament (Request $request) {
         $id = $request->get('tournament-id');
-        $tournament = $this->entityManger->getRepository('App:TFTournament')->find($id);
+        $tournament = $this->entityManager->getRepository('App:TFTournament')->find($id);
 
         if($tournament) {
-            $this->entityManger->remove($tournament);
-            $this->entityManger->flush();
+            $this->entityManager->remove($tournament);
+            $this->entityManager->flush();
             $this->addFlash('success', 'tournament.remove.message');
         }else{
             $this->addFlash('warning', 'tournament.remove.message');
@@ -97,75 +98,32 @@ class TournamentController extends Controller
     /**
      * @Route("/tournament/{tournamentId}/addParticipant", name="add-participant", requirements={"\s"})
      */
-    public function addParticipant (Request $request, string $tournamentId)
+    public function addParticipant (Request $request, string $tournamentId, TournamentService $tournamentService)
     {
-        /**
-         * @var TFTournamentRepository $repo
-         */
-        $repo = $this->entityManger->getRepository(TFTournament::class);
+        /* @var TFTournamentRepository $repo */
+        $repo = $this->entityManager->getRepository(TFTournament::class);
         $tournament = $repo->find($tournamentId);
-        /**
-         * @var TFUser[] $players_in_tournament
-         */
-        $players_in_tournament = $tournament->getPlayers()->toArray();
-
 
         if($tournament == null){
             throw new NotFoundHttpException("Ce tournoi n'existe pas");
         }
 
-        /**
-         * @var  TFUserRepository $repo
-         */
-        $repo = $this->entityManger->getRepository(TFUser::class);
-        /**
-         * @var Collection $users
-         */
+        /* @var TFUser[] $players_in_tournament */
+        $tournamentPlayers = $tournament->getPlayers()->toArray();
+
+        /* @var  TFUserRepository $repo */
+        $repo = $this->entityManager->getRepository(TFUser::class);
+        /* @var Collection $users */
         $users = $repo->findAll();
 
-        $form = $this->createForm('App\Form\Type\AddParticipantToTournamentType', $tournament, ['users' => $users, 'players' => $players_in_tournament]);
+        $form = $this->createForm('App\Form\Type\AddParticipantToTournamentType', $tournament, ['users' => $users, 'players' => $tournamentPlayers]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
-            /**
-             * @var ArrayCollection $submited
-             */
+            /* @var array $submited */
             $submited = $request->get('add_participant_to_tournament');
-            if(array_key_exists('players',$submited)){
-                $players_id = $submited['players'];
-            }else{
-                $players_id = [];
-            }
-            $players = new ArrayCollection();
-            foreach($players_id as $id){
-                $players->add($repo->find($id));
-            }
-            $players_to_delete = array_diff($players_in_tournament, $players->toArray());
-            /**
-             * @var TFUser $player
-             */
-            foreach ($players_to_delete as $player){
-                $player->removeTournaments($tournament);
-                $this->entityManger->persist($player);
-                $this->entityManger->flush();
-            }
-            foreach ($players_id as $id){
-                /**
-                 * @var TFUser $user
-                 */
-                $user = $repo->find($id);
-                /**
-                 * @var TFTournament[] $tournaments
-                 */
-                $tournaments = $user->getTournaments();
+            $tournamentService->updateTournamentParticipant($submited, $tournament);
 
-                if(! $tournaments->contains($tournament)){
-                    $user->addTournaments($tournament);
-                    $this->entityManger->persist($user);
-                    $this->entityManger->flush();
-                }
-
-            }
             $this->addFlash('success', 'tournament.update.participant');
 
             return $this->redirectToRoute('my_tournament');
