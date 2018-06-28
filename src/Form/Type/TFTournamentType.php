2ba3dff9845cae92d4f8af8ea3eb1884622e83ee
JeremLa <jeremy.lahore@gmail.com>
2018-06-28 15:47:07 +0200

    MIXED : update some design on my tournament page

--- src/Form/Type/TFTournamentType.php
+++ src/Form/Type/TFTournamentType.php
@@ -7,6 +7,7 @@ use App\Services\Enum\TournamentTypeEnum;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +16,12 @@ class TFTournamentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'tournament.form.name'
+            ])
             ->add('maxParticipantNumber', NumberType::class, [
-                'scale' => 0
+                'label' => 'tournament.form.maxParticipant',
+                'scale' => 0,
             ])
         ;
     }
